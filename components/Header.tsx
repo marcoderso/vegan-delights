5710e686d91282997264d84f94f5b07f5ea009d0
marcoderso <104020027+marcoderso@users.noreply.github.com>
2025-04-02 22:04:00 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header className="absolute top-[40px] left-0 right-0 z-[60]">
-      <div className="container mx-auto">
+     <div className="px-4 sm:px-8 xl:px-12">
         <div className="flex justify-between items-center">
           {/* logo */}
           <Link
